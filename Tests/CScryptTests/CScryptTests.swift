b7ea1349f84fbc61a884c46c4250a59b16b72119
Joonas Myhrberg <joonas@myhrberg.email>
2019-07-16 12:10:09 +0300

    Add scrypt library

--- Tests/CScryptTests/CScryptTests.swift
+++ Tests/CScryptTests/CScryptTests.swift
@@ -6,7 +6,7 @@ final class CScryptTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CScrypt().text, "Hello, World!")
+//        XCTAssertEqual(CScrypt().text, "Hello, World!")
     }
 
     static var allTests = [
